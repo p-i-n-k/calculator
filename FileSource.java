1318f5b7a27a6459ea392d5f08dbf835a85bf109
Anton <stady.mail.itmo@gmail.com>
2021-09-20 12:01:48 +0300

    new

---
+++ FileSource.java
@@ -0,0 +1,38 @@
+package calculator;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileSource implements Source {
+    private final FileInputStream data;
+    private int c;
+    private final int EOF = -1;
+
+    public FileSource(FileInputStream data) {
+        this.data = data;
+    }
+
+    @Override
+    public void nextChar() {
+        try {
+            c = c == EOF ? EOF : data.read();
+        } catch (IOException e) {
+            c = EOF;
+        }
+        
+    }
+
+    @Override
+    public char getChar() {
+        if (c != EOF) {
+            return (char)c;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return c != EOF;
+    }
+    
+}
---
+++ Test.java
@@ -0,0 +1,26 @@
+package calculator;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new FileInputStream("test.in"));
+        Scanner test = new Scanner(new FileInputStream("test.out"));
+        int testNumber = 1;
+        while (in.hasNextLine()) {
+            String data = in.nextLine();
+            long testAns = new Calc(new StringSource(data.toCharArray())).cout();
+            long ans = test.nextInt();
+            System.out.print("Test " + testNumber + ": ");
+            if (ans  == testAns) {
+                System.out.println("passed");
+            } else {
+                System.out.println("failed expected: " + ans + " actual: " + testAns);
+            }
+            testNumber++;
+        }
+        //System.out.println(calc.cout());
+    }
+}
